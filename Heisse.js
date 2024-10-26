3a5b9316968a23382884b3d0f922f2d14a0db1e8
Fikica638 <filipcevel@gmail.com>
2024-10-26 09:22:48 +0200

    a

---
+++ Heisse.js
@@ -0,0 +1,7 @@
+var e = "Filip"
+var z = "Vater"
+document.getElementById("a").innerHTML = e.toUpperCase()
+for (var i=100; i>0; i--) {
+    document.write(i, "<br>")
+    document.getElementById("b").innerHTML = i
+}
